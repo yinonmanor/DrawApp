6cf56fb28543d9b2b00505721a0e936de8304892
yinonmanor <yinonmanor@YINONMANOR029B>
2017-04-06 16:00:18 +0300

    first load

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.circleButton = new System.Windows.Forms.Button();
+            this.radiusTextBox = new System.Windows.Forms.TextBox();
+            this.xCenterTextBox = new System.Windows.Forms.TextBox();
+            this.yCenterTextBox = new System.Windows.Forms.TextBox();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(768, 569);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // circleButton
+            // 
+            this.circleButton.Location = new System.Drawing.Point(845, 12);
+            this.circleButton.Name = "circleButton";
+            this.circleButton.Size = new System.Drawing.Size(75, 23);
+            this.circleButton.TabIndex = 1;
+            this.circleButton.Text = "drow circle";
+            this.circleButton.UseVisualStyleBackColor = true;
+            this.circleButton.Click += new System.EventHandler(this.circleButton_Click);
+            // 
+            // radiusTextBox
+            // 
+            this.radiusTextBox.Location = new System.Drawing.Point(834, 41);
+            this.radiusTextBox.Name = "radiusTextBox";
+            this.radiusTextBox.Size = new System.Drawing.Size(100, 20);
+            this.radiusTextBox.TabIndex = 2;
+            // 
+            // xCenterTextBox
+            // 
+            this.xCenterTextBox.Location = new System.Drawing.Point(834, 67);
+            this.xCenterTextBox.Name = "xCenterTextBox";
+            this.xCenterTextBox.Size = new System.Drawing.Size(100, 20);
+            this.xCenterTextBox.TabIndex = 3;
+            // 
+            // yCenterTextBox
+            // 
+            this.yCenterTextBox.Location = new System.Drawing.Point(834, 93);
+            this.yCenterTextBox.Name = "yCenterTextBox";
+            this.yCenterTextBox.Size = new System.Drawing.Size(100, 20);
+            this.yCenterTextBox.TabIndex = 4;
+            // 
+            // colorButton
+            // 
+            this.colorButton.Location = new System.Drawing.Point(828, 381);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(91, 23);
+            this.colorButton.TabIndex = 5;
+            this.colorButton.Text = "choose color";
+            this.colorButton.UseVisualStyleBackColor = true;
+            this.colorButton.Click += new System.EventHandler(this.colorButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 571);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.yCenterTextBox);
+            this.Controls.Add(this.xCenterTextBox);
+            this.Controls.Add(this.radiusTextBox);
+            this.Controls.Add(this.circleButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button circleButton;
+        private System.Windows.Forms.TextBox radiusTextBox;
+        private System.Windows.Forms.TextBox xCenterTextBox;
+        private System.Windows.Forms.TextBox yCenterTextBox;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        PictureBox pb = new PictureBox();// new PictureBox(700, 700);
+        Bitmap bitMap = null;
+        Color currentColor = Color.Black;
+        public Form1()
+        {
+            InitializeComponent();
+            bitMap = new Bitmap(1000, 1000);
+            pictureBox1.Image = bitMap;// new Bitmap(700, 700);
+           
+            radiusTextBox.Text = "radius";
+            drawLine(100, 100, 100, 150, currentColor);
+            drawCircle(100,100,6, currentColor, bitMap);
+            drawPixel(10, 10, currentColor);       
+        }
+
+
+      
+        public void drawPixel(int x,int y,Color color)
+        {           
+            ((Bitmap)pictureBox1.Image).SetPixel(x, y, color);
+        }
+        
+        public void drawLine(int startX, int startY, int endX, int endY,Color color)
+        {
+            int countX = endX - startX;
+            int countY = endY - startY;
+            int count = countX > countY ? countX : countY;
+            int x = startX;
+            int y = startY;
+            for(int i=0;i<count;i++,x++,y++)
+            {
+                drawPixel(x,y,color);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ((Bitmap)pictureBox1.Image).SetPixel(100, 100, Color.Black);           
+        }
+
+        public void drawCircle(int xCen,int yCen,int rad,Color color,Bitmap bitMap)
+        {
+            pictureBox1.Image = bitMap;
+            int xCenter = xCen;//Convert.ToInt32(xCenterTextBox.Text);
+            int yCenter = yCen;// Convert.ToInt32(yCenterTextBox.Text);
+            int radius = rad;//Convert.ToInt32(radiusTextBox.Text);
+            int x = 0;
+            int y = radius;
+            int p = 3 - 2 * radius;
+            while (y >= x)
+            {
+                drawPixel(xCenter - x, yCenter - y, color);
+                drawPixel(xCenter - y, yCenter - x, color);
+                drawPixel(xCenter + y, yCenter - x, color);
+                drawPixel(xCenter + x, yCenter - y, color);
+                drawPixel(xCenter - x, yCenter + y, color);
+                drawPixel(xCenter - y, yCenter + x, color);
+                drawPixel(xCenter + y, yCenter + x, color);
+                drawPixel(xCenter + x, yCenter + y, color);
+                if (p < 0)
+                {
+                    p += 4 * x + 6;
+                    x++;
+                }
+                else
+                {
+                    p += 4 * (x - y) + 10;
+                    x++; y--;
+                }
+            }
+        }
+
+        public void circleButton_Click(object sender, EventArgs e)
+        {
+            //drawLine(100, 100, 100, 150, Color.Black);
+            int xCenter = Convert.ToInt32(xCenterTextBox.Text);
+            int yCenter = Convert.ToInt32(yCenterTextBox.Text);
+            int radius = Convert.ToInt32(radiusTextBox.Text);
+            drawCircle(xCenter, yCenter, radius, currentColor, bitMap);
+        }
+
+        private void colorButton_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                currentColor = colorDialog1.Color;
+            }
+        }
+    }
+}
